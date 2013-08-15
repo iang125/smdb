f1bfba181321c361cc7493650e140a953ce9d5b9
iang125 <RubyRailsIang@gmail.com>
2013-08-15 16:12:02 -0500

    chack if staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :authenticate
+
+  def authenticate
+    if Rails.env.staging?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["HTTP_USERNAME"] && password == ENV["HTTP_PASSWORD"]
+      end
+    end
+  end
+
   before_action :log_session_data
 
   def log_session_data
